c045daa6d17a3ea23a494f0f824e889953c36374
Albert M <albert.e.m@mail.ru>
2025-07-08 10:43:20 +0300

    new

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '123.123.123.123', '172.19.0.3'] 
+ALLOWED_HOSTS = ['localhost', '123.123.123.123', '172.19.0.3']
 
 
 # Application definition
