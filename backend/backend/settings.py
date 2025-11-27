5d9a3ab81ce8f5b0c4042ad6a561fc16caf37f29
Albert M <albert.e.m@mail.ru>
2025-11-27 21:20:08 +0300

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = "django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["kitegeron.ddns.net", "158.160.7.12", "localhost"]
+ALLOWED_HOSTS = ["kitegram.redirectme.net", "158.160.7.12", "localhost"]
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
